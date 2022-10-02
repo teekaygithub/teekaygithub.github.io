ef27a2f65eb02f14ae42ca639dc6c1ba7568c99c
teekaygithub <tomoyuki.kato@yahoo.com>
2022-10-02 00:36:58 -0700

    Split navbar and project section to components

---
+++ src/Navbar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function Navbar() {
+    return (
+        <header>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="#about">About</a>
+                    </li>
+                    <li>
+                        <a href="#projects">Projects</a>
+                    </li>
+                    <li>
+                        <a href="#skills">Skills</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default Navbar;
---
+++ src/Projects.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Projects() {
+    return (
+        <>
+            <section>
+                <div>
+                    <h1>Projects</h1>
+                </div>
+            </section>
+        </>
+    );
+}
+
+export default Projects;
