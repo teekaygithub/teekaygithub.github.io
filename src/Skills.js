5f394a926d1a90bdeba5d2fdf3ddd19abe187f89
teekaygithub <tomoyuki.kato@yahoo.com>
2022-10-08 12:32:28 -0700

    Restored icons in skills section

--- src/Skills.js
+++ src/Skills.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import SkillCard from './SkillCard';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faAws, faJava, faReact, faDocker, faStripe, faJs, faHtml5, faCss3} from '@fortawesome/free-brands-svg-icons';
 
 function Skills() {
     const images = new Map([
-        ["Java", <h2><i className="fab fa-java fa-3x"></i></h2>],
+        ["Java", <h2><FontAwesomeIcon icon={faJava} size="3x"></FontAwesomeIcon></h2>],
         ["Spring", <img className="img-fluid" src="../spring-logo.jpg"></img>],
-        ["React", <h2><i className="fab fa-react fa-3x"></i></h2>],
+        ["React", <h2><FontAwesomeIcon icon={faReact} size="3x"></FontAwesomeIcon></h2>],
         ["Redux", <img className="img-fluid" src="../redux.png" alt="redux logo"></img>],
         ["Thymeleaf", <img className="img-fluid" src="../thymeleaf.png" alt="thymeleaf"></img>],
-        ["AWS", <h2><i className="fab fa-aws fa-3x"></i></h2>],
-        ["Docker", <h2><i className="fab fa-docker fa-3x"></i></h2>],
-        ["Stripe", <h2><i className="fab fa-stripe fa-3x"></i></h2>],
-        ["Javascript", <h2><i className="fab fa-js-square fa-3x"></i></h2>],
-        ["HTML5", <h2><i className="fab fa-html5 fa-3x"></i></h2>],
-        ["CSS3", <h2><i className="fab fa-css3 fa-3x"></i></h2>],
+        ["AWS", <h2><FontAwesomeIcon icon={faAws} size="3x"></FontAwesomeIcon></h2>],
+        ["Docker", <h2><FontAwesomeIcon icon={faDocker} size="3x"></FontAwesomeIcon></h2>],
+        ["Stripe", <h2><FontAwesomeIcon icon={faStripe} size="3x"></FontAwesomeIcon></h2>],
+        ["Javascript", <h2><FontAwesomeIcon icon={faJs} size="3x"></FontAwesomeIcon></h2>],
+        ["HTML5", <h2><FontAwesomeIcon icon={faHtml5} size="3x"></FontAwesomeIcon></h2>],
+        ["CSS3", <h2><FontAwesomeIcon icon={faCss3} size="3x"></FontAwesomeIcon></h2>],
         ["JUnit", <img className="img-fluid" src="../junit.png"></img>],
     ]);
 
