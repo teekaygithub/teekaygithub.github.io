fdc80ada4d1793ba6035736621f6ede3ae4ed8c8
teekaygithub <tomoyuki.kato@yahoo.com>
2022-10-08 16:35:04 -0700

    Added work experience section

--- src/Aboutme.js
+++ src/Aboutme.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChargingStation, faCar, faRobot, faSatellite, faSpaceShuttle } from '@fortawesome/free-solid-svg-icons';
 
 function Aboutme() {
     return (
@@ -11,12 +13,42 @@ function Aboutme() {
                 <div id="about-mystory-container">
                     <p id="about-mystory">I started my career as an embedded software engineer and worked on applications
                         ranging from industrial-arm robotics all the way to human-rated spacecrafts. While I did enjoy doing the
-                        work of solving both hardware and software issues and integrating them together, I found even the
+                        work of solving both hardware and software issues and integrating them together, I found the
                         software work itself immensely rewarding.
                         As an embedded software engineer I have integrated software and hardware together to solve interesting
                         problems and to offer incredible capabilities to the end user. I believe my talent for working with
                         multiple disciplines will carry over to my role as a full-stack developer and I will continue to solve
-                        problems and deliver amazing products.</p>
+                        problems and deliver great products.</p>
+                    <div id="experience-container">
+                        <div className="experience-icon-container">
+                            <FontAwesomeIcon className="mb-3" icon={faSatellite} size="5x"></FontAwesomeIcon>
+                            <a href="https://www.maxar.com/" target="_blank">
+                                <h4>Maxar Technologies</h4>
+                                <p>(formerly Space Systems Loral)</p>
+                            </a>
+                        </div>
+                        <div className="experience-icon-container">
+                            <FontAwesomeIcon className="mb-3" icon={faSpaceShuttle} size="5x"></FontAwesomeIcon>
+                            <a href="https://www.lockheedmartin.com/en-us/index.html" target="_blank" style={{textDecoration: 'none'}}>
+                                <h4>Lockheed Martin</h4>
+                            </a>
+                        </div>
+                        <div className="experience-icon-container">
+                            <img className="mx-auto mb-3" src="../robotic-arm-svgrepo-com.svg" id="orst-logo"></img>
+                            <a href="https://www.adept.com/" target="_blank">
+                                <h4>Omron Robotics and Safety Technology</h4>
+                            </a>
+                        </div>
+                        <div className="experience-icon-container">
+                            <div>
+                                <FontAwesomeIcon className="mr-5" icon={faChargingStation} size="3x"></FontAwesomeIcon>
+                                <FontAwesomeIcon icon={faCar} size="5x"></FontAwesomeIcon>
+                            </div>
+                            <a href="https://freewiretech.com/" target="_blank">
+                                <h4 className="mt-3">FreeWire Technologies</h4>
+                            </a>
+                        </div>
+                    </div>
                 </div>
             </section>
         </>
